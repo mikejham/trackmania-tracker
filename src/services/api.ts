7f9b327efa8eaf0f9133ed50e0d1ddb93e74ae64
Michael Hamilton <mike.hamilton92@gmail.com>
2025-07-28 22:29:31 -0700

    Update API client to use environment variables for production deployment

--- src/services/api.ts
+++ src/services/api.ts
@@ -99,7 +99,7 @@ class ApiClient {
 
   constructor() {
     this.client = axios.create({
-      baseURL: "http://localhost:3001/api",
+      baseURL: import.meta.env.VITE_API_URL || "http://localhost:3001/api",
       headers: {
         "Content-Type": "application/json",
       },
